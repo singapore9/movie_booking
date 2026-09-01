0779777fb26d033791b2effac072dbc4513c9d11
singapore9 <sharik.and@gmail.com>
2026-09-01 12:06:37 +0200

    configure alembic

--- app/infrastructure/migrations/env.py
+++ app/infrastructure/migrations/env.py
@@ -6,6 +6,14 @@ from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
+from app.config import settings
+from app.infrastructure.database import (  # noqa: F401
+    Base,
+    MovieModel,
+    SavedMovieModel,
+    UserModel,
+)
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -19,12 +27,17 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+
+config.set_main_option(
+    "sqlalchemy.url",
+    settings.DATABASE_URL,
+)
 
 
 def run_migrations_offline() -> None:
